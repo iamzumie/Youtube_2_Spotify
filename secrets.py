92ef68e8c5966e8032b3ea81ad4f2c8bb1f355d4
iamzumie <Cuypers.Mic@gmail.com>
2020-10-17 19:40:09 +0200

    Delete secrets.py

--- secrets.py
+++
@@ -1,7 +0,0 @@
-channelId='UCHB0eF1pXgzg4ZfchhFa6wQ'
-playlist_id="PLAIL5-DD9jY2FXXtSVW5IFRKGjz_0VmTJ"
-username = '117945827'
-scopes = 'user-library-modify user-library-read'
-client_id = 'ba44fa72e0e24fe98c5d8c73fe95d9a2'
-client_secret = 'f8ad751901184e1a8ee7b053ce4ecb9b'
-redirect_uri = 'http://localhost:8888/callback/'
